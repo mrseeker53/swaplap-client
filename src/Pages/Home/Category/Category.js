4495956985a001177fb0bbc94a03ec58774457a6
mrseeker53 <mi.nayeem30@gmail.com>
2022-11-28 02:06:09 +0600

    Added Category

--- src/Pages/Home/Category/Category.js
+++ src/Pages/Home/Category/Category.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CategoryItem from './CategoryItem';
 
 const Category = () => {
+    // Declare state to set categories
+    const [categories, setCategories] = useState([]);
+
+    // Declare useEffect to get data from server & set data
+    useEffect(() => {
+        fetch('https://swaplap-server.vercel.app/category')
+            .then(res => res.json())
+            .then(data => setCategories(data));
+    }, []);
+
     return (
-        <div>
-            <h2>Category</h2>
+        <div className='my-20'>
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                {
+                    categories.map(category => <CategoryItem
+                        key={category._id}
+                        category={category}
+                    ></CategoryItem>)
+                }
+            </div>
         </div>
     );
 };
