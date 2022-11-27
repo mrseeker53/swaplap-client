cfc760b8d045a54438fa00c36fa2f5bdec0c6ed6
mrseeker53 <mi.nayeem30@gmail.com>
2022-11-27 23:02:03 +0600

    Added Banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import BannerItem from './BannerItem';
 
 const Banner = () => {
+    // Declare state to set banner data
+    const [banner, setBanner] = useState([]);
+
+    // Load data from server
+    useEffect(() => {
+        fetch('https://swaplap-server.vercel.app/banner')
+            .then(res => res.json())
+            .then(data => setBanner(data));
+    }, []);
+
     return (
-        <div>
-            <h2>Banner</h2>
+        <div className="carousel w-full py-10">
+            {
+                // Loop through on banner & send data as item to the BannerItem
+                banner.map(item => <BannerItem
+                    key={item._id}
+                    item={item}
+                ></BannerItem>)
+            }
         </div>
     );
 };
