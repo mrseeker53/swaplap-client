48d12e493fdad711fea358c9803fd4d38eecbc05
mrseeker53 <mi.nayeem30@gmail.com>
2022-11-29 01:20:42 +0600

    Added product detail

--- src/Pages/Home/Category/Product/ProductItem.js
+++ src/Pages/Home/Category/Product/ProductItem.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { PhotoProvider, PhotoView } from 'react-photo-view';
 import { Link } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
 const ProductItem = ({ product }) => {
-    const { _id, img, title, location, price, description } = product;
+    const { _id, img, title, location, originalPrice, price, useYear, seller, post, status, description } = product;
 
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
@@ -15,11 +16,17 @@ const ProductItem = ({ product }) => {
             </PhotoProvider>
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
-                <p className='text-xl font-semibold'>Price: ${price}</p>
-                <p className='text-lg'>{description.slice(0, 100)}</p>
+                <p className='text-md font-semibold'>Original Price: {originalPrice} Tk</p>
+                <p className='text-md font-semibold'>Price: {price} Tk</p>
+                <p className='text-md'>Location: {location}</p>
+                <p className='text-md'>Use of year: {useYear}</p>
+                <p className='text-md'>Seller Name: {seller}</p>
+                <p className='text-md'>Posted: {post}</p>
+                <p className='text-md'>Availability: {status}</p>
+                <p className='text-md'>{description}</p>
                 <div className="card-actions justify-end mt-8">
                     <Link to={`/menu/${_id}`}>
-                        <button className="btn btn-outline btn-primary text-white">View Details</button>
+                        <button className="btn btn-outline btn-primary">Book Now</button>
                     </Link>
                 </div>
             </div>
