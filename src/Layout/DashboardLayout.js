fb3a7c3d208ff5bd21b1a095879ac6b1e647ee5c
mrseeker53 <mi.nayeem30@gmail.com>
2022-11-30 13:03:34 +0600

    Set Admin route

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -1,11 +1,13 @@
 import React, { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { AuthContext } from '../contexts/AuthProvider';
+import useAdmin from '../hooks/useAdmin';
 import Navbar from '../Pages/Shared/Navbar/Navbar';
 
 const DashboardLayout = () => {
     // Declare context & hook
     const { user } = useContext(AuthContext);
+    const [isAdmin] = useAdmin(user?.email);
 
     return (
         <div>
@@ -21,7 +23,16 @@ const DashboardLayout = () => {
                     <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-80 bg-base-100 text-base-content">
                         <li><Link to="/dashboard">My Orders</Link></li>
-
+                        <li><Link to="/dashboard/addaproduct">Add A Product</Link></li>
+                        <li><Link to="/dashboard/myproducts">My Products</Link></li>
+                        <li><Link to="/dashboard/mybuyers">My Buyers</Link></li>
+                        {
+                            isAdmin && <>
+                                <li><Link to="/dashboard/allbuyers">All Buyers</Link></li>
+                                <li><Link to="/dashboard/allsellers">All Sellers</Link></li>
+                                <li><Link to="/dashboard/reporteditems">Reported Items</Link></li>
+                            </>
+                        }
                     </ul>
 
                 </div>
